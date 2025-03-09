4d3c8361b302d0273db3fa8d3902e314f171af11
Tanmoy Biswas <tanmoyb.cse@gmail.com>
2025-03-09 10:10:52 +0600

    Add card aded

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,24 @@
 import Button from './UI/Button.jsx';
 import logoImg from '../assets/logo.jpg';
 
+import CartContext from '../store/CartContext.jsx';
+import UserProgressContext from '../store/UserProgressContext.jsx';
+import { useContext } from 'react';
+
 export default function Header() {
+
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
+
+
   return (
     <header id="main-header">
       <div id="title">
@@ -9,7 +26,9 @@ export default function Header() {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly={true}>Cart {0} </Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
